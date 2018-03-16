b8f95f0a60a5fe756501703aeeeff0fa7d2cc206
DavidRooban <37235341+DavidRooban@users.noreply.github.com>
2018-03-16 15:03:55 +0530

    Update oddrange.py

--- oddrange.py
+++ oddrange.py
@@ -1,7 +1,7 @@
 num = int(input("Enter starting range: "))
 n=int(input("enter ending limit"))
-if(num>10000 or n>10000):
-    print("enter less than 10000")
+if(num>100000 or n>100000):
+    print("enter less than 100000")
 else:
    for i in range(num+1,n):
        if (i % 2) > 0:
