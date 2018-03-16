1a8496afa130e120b1ce750b8d0da112372e4559
DavidRooban <37235341+DavidRooban@users.noreply.github.com>
2018-03-16 22:36:11 +0530

    Update maxarray.py

--- maxarray.py
+++ maxarray.py
@@ -1,6 +1,9 @@
 list=[]
 n=int(input("enter n-limit"))
-for i in range(1,n+1):
-    a=(input("enter element"))
-    list.append(a)
-print(max(list))
+if(n>100000):
+    print("enter less than 100000")
+else:
+    for i in range(1,n+1):
+        a=(input("enter element"))
+        list.append(a)
+    print(max(list))
