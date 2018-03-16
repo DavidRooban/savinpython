d0fbdb8929700f637f73d9c0cfbf399c844f95f0
DavidRooban <37235341+DavidRooban@users.noreply.github.com>
2018-03-16 14:49:43 +0530

    Update primeornot.py

--- primeornot.py
+++ primeornot.py
@@ -1,11 +1,12 @@
 num = int(input("Enter a number: "))
-if num > 1:
+if(num>1000):
+    print("enter less than 1000")
+elif num > 1:
    for i in range(2,num):
        if (num % i) == 0:
            print("no")
            break
    else:
        print("yes")
-
 else:
    print("no")
