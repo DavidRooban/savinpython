02cc0661c9cfce4a9a6a408f58ec9096ba62cc38
DavidRooban <37235341+DavidRooban@users.noreply.github.com>
2018-03-16 15:06:51 +0530

    Create evenrange.py

---
+++ evenrange.py
@@ -0,0 +1,8 @@
+num = int(input("Enter starting range: "))
+n=int(input("enter ending limit"))
+if(num>10000 or n>10000):
+    print("enter less than 10000")
+else:
+   for i in range(num+1,n):
+       if (i % 2) == 0:
+           print(i)
