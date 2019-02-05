cc9ecc5a388c41fd95fce2b6795dad48c9664098
DavidRooban <37235341+DavidRooban@users.noreply.github.com>
2019-02-05 15:15:37 +0530

    Update con or vowel.py

--- con or vowel.py
+++ con or vowel.py
@@ -1,6 +1,8 @@
 list=['a','e','i','o','u','A','E','I','O','U']
-r=raw_input("value is:")
+r=raw_input()
 if(r in list):
-    print"vowel"
+    print"Vowel"
+elif r.isalpha():
+  print"Consonant"
 else:
-    print"Consonant"
+  print("invalid")
