bf222bbedf124c93db301226f0046e2eefaddf9a
DavidRooban <37235341+DavidRooban@users.noreply.github.com>
2019-08-08 11:10:03 +0530

    Update min arr.py

--- min arr.py
+++ min arr.py
@@ -1,9 +1,3 @@
-list=[]
-n=int(input("enter n-limit"))
-if(n>100000):
-    print("enter less than 100000")
-else:
-    for i in range(1,n+1):
-        a=(input("enter element"))
-        list.append(a)
-    print(min(list))
+n=int(input())
+a = list(map(int,input().split()))[:n]
+print(min(a))
