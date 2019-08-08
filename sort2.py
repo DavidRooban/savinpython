0012ac0721fb91d9ec86d4e32a8862008aaf3fc9
DavidRooban <37235341+DavidRooban@users.noreply.github.com>
2019-08-08 11:47:54 +0530

    Update sort2.py

--- sort2.py
+++ sort2.py
@@ -1,10 +1,4 @@
-list=[]
-n=int(input("enter n-limit:"))
-if(n>10000):
-    print("enter less than 10000")
-else:
-    for i in range(1,n+1):
-        a=(input("enter element :"))
-        list.append(a)
-        list.sort()
-print '\n'.join([ str(a) for a in list ])
+n=int(input())
+a = list(map(int,input().split()))[:n]
+a.sort()
+print(*a)
