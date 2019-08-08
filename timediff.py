320d17ea90009f81c44812b9c833d65bd9bce73b
DavidRooban <37235341+DavidRooban@users.noreply.github.com>
2019-08-08 12:28:12 +0530

    Update timediff.py

--- timediff.py
+++ timediff.py
@@ -1,3 +1,4 @@
-a,b=map(int,input().split())
-c,d=map(int,input().split())
-print(abs(a-c),abs(b-d))
+a1,b1=map(int,input().split())
+c1,d1=map(int,input().split())
+if (a1<13 and b1<13):
+    print(abs(a1-c1),abs(b1-d1))
