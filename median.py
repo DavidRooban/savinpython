ec8fb4a5bba6338ea1e8970bdaff154c3bfd3870
DavidRooban <37235341+DavidRooban@users.noreply.github.com>
2019-08-08 11:41:18 +0530

    Update median.py

--- median.py
+++ median.py
@@ -1,14 +1,5 @@
-list=[]
-n=int(input("enter n-limit"))
-if(n>1000):
-    print("enter less than 1000")
-else:
-    for i in range(1,n+1):
-        a=(input("enter element"))
-        list.append(a)
-        list.sort()
-    if(n%2>0):
-        print(list[(n//2)])
-    else:
-        print("greater median is")
-        print(list[(n//2)])
+import statistics
+n=int(input())
+a = list(map(int,input().split()))[:n]
+a.sort()
+print(statistics.median(a))
